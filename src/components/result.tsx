522d00dac416efadeb16f42accbb841fd4849375
Hugo Martinet <59569523+hugomartinet@users.noreply.github.com>
2023-11-27 22:46:21 +0100

    add santa after result

--- src/components/result.tsx
+++ src/components/result.tsx
@@ -1,26 +1,42 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Image, Slide, Text } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import ReactConfetti from 'react-confetti';
 
+import santaImage from '../assets/santa.png';
 import { options } from '../const';
 import { useWheelContext } from '../context/wheel-context';
 
 export function Result() {
   const { result } = useWheelContext();
 
+  const [showSanta, setShowSanta] = useState(false);
+
+  useEffect(() => {
+    if (result) setTimeout(() => setShowSanta(true), 2000);
+  }, [result]);
+
   return result ? (
-    <Box
-      position="absolute"
-      top="50%"
-      left="50%"
-      zIndex={3}
-      transform="rotate(-15deg) translate(-50%,-50%)"
-      padding={8}
-      bg="white"
-      shadow="lg"
-      textAlign="center"
-    >
-      <Text fontSize="5xl" fontWeight="bold">
-        {options.find(option => option.label === result)?.description}
-      </Text>
-    </Box>
+    <>
+      <Slide direction="left" in={showSanta}>
+        <ReactConfetti width={window.innerWidth} height={window.innerHeight} />
+        <Image src={santaImage} />
+      </Slide>
+
+      <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        zIndex={3}
+        transform="rotate(-15deg) translate(-50%,-50%)"
+        padding={8}
+        bg="white"
+        shadow="lg"
+        textAlign="center"
+      >
+        <Text fontSize="5xl" fontWeight="bold">
+          {options.find(option => option.label === result)?.description}
+        </Text>
+      </Box>
+    </>
   ) : null;
 }
